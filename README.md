a9202579a714e31432163788c8ddd26614b8f410
lj2508-com <admin@lj2508.com>
2022-03-20 23:29:25 +0800

    新增备注，和登录接口

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-im-v2
-自练手的im项目
+自练手的im项目   --
+参考自慕课网
+
+https://coding.imooc.com/class/339.html
--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/user/login", func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer, "测试接口")
-	})
+	http.HandleFunc("/user/login", userlogin)
 	http.ListenAndServe(":8090", nil)
+}
+
+func userlogin(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	mobile := request.PostForm.Get("mobile")
+	passwd := request.PostForm.Get("passwd")
+	loginSu := false
+	if mobile == "18100000000" && passwd == "123456" {
+		loginSu = true
+	}
+	if loginSu {
+		data := make(map[string]interface{})
+		data["id"] = 1
+		data["token"] = "test"
+		resp(writer, 0, data, "")
+	} else {
+		resp(writer, -1, nil, "账号或密码错误！")
+	}
+}
+
+type respBady struct {
+	Code int
+	Data interface{}
+	Msg  string
+}
+
+func resp(writer http.ResponseWriter, code int, data interface{}, msg string) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	bady := respBady{
+		Code: code,
+		Data: data,
+		Msg:  msg,
+	}
+	json, _ := json.Marshal(bady)
+	writer.Write(json)
 }
